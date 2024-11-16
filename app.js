0b2250157cc9e02a25624686e8955852b02f4c98
Mr Dipayan Dey <161328487+Dipayan-Dey@users.noreply.github.com>
2024-11-16 19:47:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const filterbtn = document.querySelectorAll(".btns button");
+  const gallary = document.querySelectorAll(".gallary .item");
+
+  filterbtn.forEach((buttons) => {
+    buttons.addEventListener("click", function () {
+      const value = this.getAttribute("data-filter");
+
+      gallary.forEach((item) => {
+        if (value === "all" || item.classList.contains(value)) {
+          item.style.display = "block";
+        } else {
+          item.style.display = "none";
+        }
+      });
+    });
+  });
+});
